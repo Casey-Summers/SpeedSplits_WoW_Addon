41afbda98aac661cc202c04db075b6db3f17384f
Casey Summers <casey.summers@my.jcu.edu.au>
2026-01-25 14:48:55 +1000

    Adds a timer frame and timer

--- SpeedSplits.lua
+++ SpeedSplits.lua
@@ -5,7 +5,8 @@ print("SpeedSplits successfully loaded!")
 -- List of default values for variables
 local DefaultDB = {
     bossKills = 0, -- tracks total boss kills
-    bossKillHistory = {} -- {{id, name, split}}
+    bossKillHistory = {}, -- {{id, name, split}}
+    runs = {} -- saves splits per instance, per run
 }
 
 local function ActivateDB()
@@ -58,8 +59,38 @@ mainFrame.totalBossKills = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontN
 mainFrame.totalBossKills:SetPoint("TOPLEFT", mainFrame.playerName, "BOTTOMLEFT", 0, -50)
 mainFrame.totalBossKills:SetText("Total boss kills: " .. (DB.bossKills))
 
+-- Timer frame
+local timerFrame = CreateFrame("Frame", "SpeedSplitsTimerFrame", UIParent, "BasicFrameTemplateWithInset")
+timerFrame:SetSize(200, 80)
+timerFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+timerFrame:Show()
+
+timerFrame.TitleBg:SetHeight(30)
+timerFrame.title = timerFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+timerFrame.title:SetPoint("TOPLEFT", timerFrame.TitleBg, "TOPLEFT", 5, -3)
+timerFrame.title:SetText("Run Timer")
+
+timerFrame.timeText = timerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+timerFrame.timeText:SetPoint("CENTER", timerFrame, "CENTER", 0, -8)
+timerFrame.timeText:SetText("0:00.000")
+
+timerFrame:EnableMouse(true)
+timerFrame:SetMovable(true)
+timerFrame:RegisterForDrag("LeftButton")
+timerFrame:SetScript("OnDragStart", function(self)
+    self:StartMoving()
+end)
+timerFrame:SetScript("OnDragStop", function(self)
+    self:StopMovingOrSizing()
+end)
+
 -- Creates an invisible event listener frame
 local eventListenerFrame = CreateFrame("Frame", "SpeedSplitsEventListenerFrame", UIParent)
+
+-- === Timer State ===
+local pendingRunStart = false
+local timerRunning = false
+local runStartTime = nil
 
 -- === Boss Kill Logic ===
 -- Stores every boss kill line added (so they can be stacked)
@@ -92,9 +123,64 @@ local function addBossKill(encounterID, encounterName)
     nextLineNumber = nextLineNumber + 1
 end
 
+local function FormatTime(seconds)
+    local minutes = math.floor(seconds / 60)
+    local remainder = seconds - (minutes * 60)
+    return string.format("%d:%06.3f", minutes, remainder)
+end
+
+local function StartTimer()
+    if timerRunning then
+        return
+    end
+
+    runStartTime = GetTime()
+    timerRunning = true
+
+    timerFrame:SetScript("OnUpdate", function()
+        timerFrame.timeText:SetText(FormatTime(GetTime() - runStartTime))
+    end)
+end
+
+local function StopTimer()
+    timerFrame:SetScript("OnUpdate", nil)
+    pendingRunStart = false
+    timerRunning = false
+    runStartTime = nil
+    timerFrame.timeText:SetText("0:00.000")
+end
+
 -- === Events ===
 -- when event condition is met, call this funciton
 local function eventHandler(_, event, ...)
+    if event == "PLAYER_ENTERING_WORLD" then
+        local inInstance = IsInInstance()
+
+        if inInstance then
+            pendingRunStart = true
+            print("Entered instance: timer armed (waiting for movement).")
+
+            -- edge case: if you reload while already moving, start immediately
+            if GetUnitSpeed("player") > 0 then
+                pendingRunStart = false
+                StartTimer()
+            end
+        else
+            StopTimer()
+            print("left instance: timer cleared.")
+        end
+        return
+    end
+
+    if event == "PLAYER_STARTED_MOVING" then
+        if pendingRunStart and IsInInstance() then
+            pendingRunStart = false
+            StartTimer()
+            print("Moved in instance: timer started.")
+        end
+        return
+    end
+
     if event == "BOSS_KILL" then
         -- BOSS_KILL args come from ... 
         local encounterID, encounterName = ...
@@ -114,6 +200,8 @@ end
 -- Registers the event frame to the event, and what function should be called 
 eventListenerFrame:SetScript("OnEvent", eventHandler)
 eventListenerFrame:RegisterEvent("BOSS_KILL")
+eventListenerFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+eventListenerFrame:RegisterEvent("PLAYER_STARTED_MOVING")
 
 -- === Slash Commands ===
 -- Commands to show addon
@@ -122,10 +210,13 @@ SLASH_SPEEDSPLITS2 = "/ss"
 SlashCmdList["SPEEDSPLITS"] = function()
     if mainFrame:IsShown() then
         mainFrame:Hide()
+        timerFrame:Hide()
     else
         mainFrame:Show()
+        timerFrame:Show()
     end
 end
 
 -- Allows addon to be exited with ESC
-table.insert(UISpecialFrames, "SpeedSplitsMainFrame")
+-- table.insert(UISpecialFrames, "SpeedSplitsMainFrame")
+-- table.insert(UISpecialFrames, "SpeedSplitsTimerFrame")
